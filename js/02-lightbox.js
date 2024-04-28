35620e99a9ef3d70205b26deb1fdb7201ce67d30
GosiaDabrowska <d.dabrowska.m@gmail.com>
2024-04-29 00:29:54 +0200

    improved 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,11 +7,10 @@ const galleryContainer = document.querySelector(`.gallery`);
 
 function createGalleryItem(image) {
     return `<li class="gallery__item">
-    <a class="gallery__link" href="">
+    <a class="gallery__link" href="${image.original}">
       <img
         class="gallery__image"
         src="${image.preview}"
-        data-source="${image.original}"
         alt="${image.description}"
       />
     </a>
